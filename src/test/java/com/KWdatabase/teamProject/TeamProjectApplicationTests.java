306c8c4b197a64043862ecfa3694d7eda75ce7b7
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2022-11-10 16:00:45 +0900

    Re-upload for Building an Initial Environment (#2)
    * Building an Initial Environment
    
    * Create README.md
    
    * Building an Initial Environment

---
+++ src/test/java/com/KWdatabase/teamProject/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KWdatabase.teamProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
