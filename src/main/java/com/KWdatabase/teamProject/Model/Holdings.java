ff2a1e775d6f34f1ad14008fe43739a8d44f789b
jiwon <83341978+jiione@users.noreply.github.com>
2022-11-15 13:08:03 +0900

    . (#13)

---
+++ src/main/java/com/KWdatabase/teamProject/Model/Holdings.java
@@ -0,0 +1,8 @@
+package com.KWdatabase.teamProject.Model;
+
+public class Holdings {
+    private String itemCode;
+    private String id;
+    private int itemNumber;
+    private float bidPrice;
+}
