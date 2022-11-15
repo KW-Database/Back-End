f62c4f3306e9b7148e20bc7eb7958d3237f5456e
jiwon <83341978+jiione@users.noreply.github.com>
2022-11-15 19:50:18 +0900

    Feature#15 profile기능 (#16)
    * User 조회,등록 기능
    
    * 회원 프로필 기능 구현

---
+++ src/test/java/com/KWdatabase/teamProject/dao/UserDaoTest.java
@@ -0,0 +1,49 @@
+package com.KWdatabase.teamProject.dao;
+
+import com.KWdatabase.teamProject.Model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class UserDaoTest {
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void signUp_Test() throws Exception {
+
+        int adminAuth = 1;
+        int age = 24;
+        String id = "Jiwon";
+        String email="pg15320@gmail.com";
+        String phoneNumber="01012341234";
+        String pw="1234";
+        char sex = 'M';
+        String username = "ParkJiwon";
+
+        User user = User.builder()
+                .adminAuth(adminAuth)
+                .age(age)
+                .id(id)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .pw(pw)
+                .sex(sex)
+                .username(username)
+                .build();
+
+        userDao.signUp(user);
+
+        User user1 =userDao.findUser(user.getId());
+
+        assertThat(user1.getId()).isEqualTo(user.getId());
+
+    }
+}
