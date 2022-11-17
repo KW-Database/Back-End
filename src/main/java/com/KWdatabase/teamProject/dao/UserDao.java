a4a725c4198898a1133a4c50bc1724606abe9e5b
jiwon <83341978+jiione@users.noreply.github.com>
2022-11-17 15:59:49 +0900

    Feature#15회원정보 (#24)
    * Create User.xml
    
    * Update User.xml
    
    * Create UserDao.java
    
    * Create ProfileController.java
    
    * create UserService

---
+++ src/main/java/com/KWdatabase/teamProject/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.KWdatabase.teamProject.dao;
+
+import com.KWdatabase.teamProject.Model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserDao {
+    public User findUser(String id);
+    public void signUp(@Param("user") User user);
+    public void updateUser(@Param("user") User user);
+    public void deleteUser(String id);
+}
