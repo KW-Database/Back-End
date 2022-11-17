ee2bbb72089df47efedcf39720a48b7bdb522a4f
민선 <pg15320@gmail.com>
2022-11-17 13:37:18 +0900

    .

--- src/main/java/com/KWdatabase/teamProject/controller/ProfileController.java
+++ src/main/java/com/KWdatabase/teamProject/controller/ProfileController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
+
     private final UserService userService;
 
     @GetMapping("/info")
