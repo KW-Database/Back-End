1cca04aa34df30d4979a6b087c0f5e360ee8d241
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2022-12-02 14:50:41 +0900

    Shareholder 구현

--- src/main/java/com/KWdatabase/teamProject/Model/Shareholder.java
+++ src/main/java/com/KWdatabase/teamProject/Model/Shareholder.java
@@ -1,10 +1,12 @@
 package com.KWdatabase.teamProject.Model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Shareholder {
     private String itemCode;
     private String majorHolders;
--- src/main/java/com/KWdatabase/teamProject/dao/ShareholderDao.java
+++ src/main/java/com/KWdatabase/teamProject/dao/ShareholderDao.java
@@ -1,7 +1,18 @@
 package com.KWdatabase.teamProject.dao;
 
+import com.KWdatabase.teamProject.Model.Shareholder;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ShareholderDao {
+    public List<Shareholder> getShareholderList(String itemCode);
+
+    public void insertShareholderData(@Param("sh")Shareholder shareholder);
+
+    public void updateShareholderData(@Param("sh")Shareholder shareholder,@Param("ch")Shareholder changes);
+
+
 }
