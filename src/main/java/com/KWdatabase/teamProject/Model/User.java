79c47b79738a9e24ec75f3ff6ea59981c93b67a7
jiwon <pg15320@gmail.com>
2022-12-04 11:57:04 +0900

    로그인 구현

--- src/main/java/com/KWdatabase/teamProject/Model/User.java
+++ src/main/java/com/KWdatabase/teamProject/Model/User.java
@@ -3,11 +3,19 @@ package com.KWdatabase.teamProject.Model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
-public class User {
+public class User implements UserDetails {
     private String id;
     private String pw;
     private String username;
@@ -17,4 +25,43 @@ public class User {
     private char sex;
     private long cash;
     private String adminAuth;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority(adminAuth));
+
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return pw;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 우리 사이트에서 1년동안 회원이 로그인을 안하면 휴면계정으로 하기로 했다면
+        return true;
+    }
 }
