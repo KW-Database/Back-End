5a4ecb064d8e5bf4a265f5624daeffde7f8fc411
jiwon <83341978+jiione@users.noreply.github.com>
2022-12-06 04:05:48 +0900

    로그인 구현 (#63)

--- src/main/java/com/KWdatabase/teamProject/Service/UserService.java
+++ src/main/java/com/KWdatabase/teamProject/Service/UserService.java
@@ -3,13 +3,26 @@ package com.KWdatabase.teamProject.Service;
 import com.KWdatabase.teamProject.Model.User;
 import com.KWdatabase.teamProject.dao.UserDao;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserDao userDao;
 
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+
+        User user = userDao.findUser(id);
+
+        if( user == null ) {
+            throw new UsernameNotFoundException(id);
+        }
+        return user;
+    }
     public User findUser(String id){
         return userDao.findUser(id);
     }
