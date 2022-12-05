f392dc8b53e4c92e74cf78f02c99d1cdc3a9cd8f
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2022-12-06 01:14:05 +0900

    Exchange 기업개요, 보유주식, 그래프, 연령대별주식, 보유금액정보 (#60)
    * 수정
    
    * Exchange : 기업개요 추가 및 그래프
    
    * Exchange :myWalletInfo
    
    * Exchange : 보유주식

--- src/main/java/com/KWdatabase/teamProject/controller/ExchangeController.java
+++ src/main/java/com/KWdatabase/teamProject/controller/ExchangeController.java
@@ -1,20 +1,53 @@
 package com.KWdatabase.teamProject.controller;
 
-import com.KWdatabase.teamProject.Model.BuyRequestDto;
-import com.KWdatabase.teamProject.Service.ExchangeService;
+import com.KWdatabase.teamProject.Model.*;
+import com.KWdatabase.teamProject.Service.*;
+import com.KWdatabase.teamProject.dao.CompanyDao;
+import com.KWdatabase.teamProject.dao.HoldingsDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exchange")
 public class ExchangeController {
     private final ExchangeService exchangeService;
+    private final CompanyDao companyDao;
+    private final ItemDayConditionService itemDayConditionService;
+    private final HolderAgeService holderAgeService;
+    private final MyWalletService myWalletService;
+    private final HoldingsDao holdingsDao;
     @PostMapping("/buy")
     public ResponseEntity<HttpStatus> buy(@RequestBody BuyRequestDto buyRequestDto){
         exchangeService.buy(buyRequestDto);
         return ResponseEntity.ok().body(HttpStatus.OK);
     }
+
+    @GetMapping
+    public ResponseEntity<Map<String,Object>> info(@RequestBody Map<String, Object> json ){
+        String itemCode = json.get("itemCode").toString();
+        String id = json.get("id").toString();
+
+        Company company= companyDao.getCompany(itemCode);
+        Map<String,Object> map = new HashMap<>();
+        map.put("companyInfo", company);
+        List<ItemDayCondition> itemDayConditionList = itemDayConditionService.getItemDayCondition(itemCode);
+        map.put("dayCondition", itemDayConditionList);
+        List<HolderAge> holderAgeList = holderAgeService.getData(itemCode);
+        map.put("holderAge", holderAgeList);
+        MyWalletResponseDto myWalletResponseDto = myWalletService.getMyWallet(id);
+        map.put("myWalletInfo", myWalletResponseDto);
+        List<Holdings> holdings = holdingsDao.getHoldings(id);
+        map.put("holdings", holdings);
+
+        return ResponseEntity.ok().body(map);
+    }
+
+
 }
