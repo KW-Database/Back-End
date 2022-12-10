38f043a63921b2c83c2ac8b7d97d2e606a960377
jiwoo0629 <90768869+jiwoo0629@users.noreply.github.com>
2022-12-11 02:36:13 +0900

    .

--- src/main/java/com/KWdatabase/teamProject/TeamProjectApplication.java
+++ src/main/java/com/KWdatabase/teamProject/TeamProjectApplication.java
@@ -2,13 +2,9 @@ package com.KWdatabase.teamProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-=======
-
->>>>>>> origin/feature
 @SpringBootApplication
 public class TeamProjectApplication {
 
